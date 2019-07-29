dae914ebd57fe416a719f4cc9525733d633d010d
mayankphilips <mayank.jaguri@philips.com>
2019-07-29 12:00:17 +0530

    Updating Alerting Module

--- vitalCheck.java
+++ vitalCheck.java
@@ -15,8 +15,6 @@ public class vitalCheck {
 		
 		if( pulseRate >=50 && pulseRate <= 100 ) { 
 			
-			
-			System.out.println("No Alert");
 			return pulseRateNormalLimit; //Normal Status and no alarm needs to be raised
 			
 		}
@@ -24,27 +22,27 @@ public class vitalCheck {
 		
 		
 		else if( pulseRate >=40 && pulseRate < 50 ){
-			System.out.println("Alert");
+			
 			return lowPulseRateWarningLimit; //Alarming Drop and Attention Needed
 			
 		}
 		
 		
 		else if( pulseRate >=30 && pulseRate < 40) {
-			System.out.println("Alert");
+			
 			return lowPulseRateCriticalLimit; //Critical Level Drop in SPO2 , Immediate Response Required / Alert Doctor
 			
 		}
 		
 		else if( pulseRate >100 && pulseRate < 120 ){
-			System.out.println("Alert");
+			
 			return highPulseRateWarningLimit; //Alarming Drop and Attention Needed
 			
 		}
 		
 		
 		else if( pulseRate >=120 && pulseRate < 254) {
-			System.out.println("Alert");
+			
 			return highPulseRateCriticalLimit; //Critical Level Drop in SPO2 , Immediate Response Required / Alert Doctor
 			
 		}
@@ -68,7 +66,7 @@ public class vitalCheck {
 		final int tempratureMachineError = 1;
 		
 		if( tempratureF >=97.0 && tempratureF <= 99.00 ) { 
-			System.out.println("No Alert");
+			
 			return tempratureNormalLimit; //Normal Status and no alarm needs to be raised
 			
 		}
@@ -76,27 +74,27 @@ public class vitalCheck {
 		
 		
 		else if( tempratureF >=95.00 && tempratureF < 97.00 ){
-			System.out.println("Alert");
+			
 			return lowTempratureWarningLimit; //Alarming Drop and Attention Needed
 			
 		}
 		
 		
 		else if( tempratureF >=93.00 && tempratureF < 95.00) {
-			System.out.println("Alert");
+			
 			return lowTempratureCriticalLimit; //Critical Level Drop in SPO2 , Immediate Response Required / Alert Doctor
 			
 		}
 		
 		else if( tempratureF >99.00 && tempratureF < 104.00 ){
-			System.out.println("Alert");
+			
 			return highTempratureWarningLimit; //Alarming Drop and Attention Needed
 			
 		}
 		
 		
 		else if( tempratureF >=104.00 && tempratureF < 108.00) {
-			System.out.println("Alert");
+			
 			return highTempratureCriticalLimit; //Critical Level Drop in SPO2 , Immediate Response Required / Alert Doctor
 			
 		}
@@ -118,7 +116,7 @@ public class vitalCheck {
 		final int oxygenMachineError = 1;
 		
 		if( oxygenConcentration >=91 && oxygenConcentration <= 100 ) { 
-			System.out.println("No Alert");
+			
 			return oxygenNormalLimit; //Normal Status and no alarm needs to be raised
 			
 		}
@@ -126,14 +124,14 @@ public class vitalCheck {
 		
 		
 		else if( oxygenConcentration >=81 && oxygenConcentration < 91 ){
-			System.out.println("Alert");
+			
 			return oxygenWarningLimit; //Alarming Drop and Attention Needed
 			
 		}
 		
 		
 		else if( oxygenConcentration >=70 && oxygenConcentration < 81) {
-			System.out.println("Alert");
+			
 			return oxygenCriticalLimit; //Critical Level Drop in SPO2 , Immediate Response Required / Alert Doctor
 			
 		}
