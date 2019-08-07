a8cf8a2370d6e2c94f9ee09da3628dfe51fa8e25
HarshithaYeddula <33006130+HarshithaYeddula@users.noreply.github.com>
2019-08-07 17:51:03 +0530

    Update TestSuite.java

--- src/TestCases/TestSuite.java
+++ src/TestCases/TestSuite.java
@@ -4,4 +4,5 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({TestValidatingModule.class, TestPulseRate.class,TestTemprature.class,TestOxygenConcentration.class})
-public class TestSuite{}
+public class TestSuite{
+}
