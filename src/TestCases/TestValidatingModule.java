79e0737dfc8a69236848a996707ec308b2ada0e6
HarshithaYeddula <33006130+HarshithaYeddula@users.noreply.github.com>
2019-08-07 17:52:04 +0530

    Update TestValidatingModule.java

--- src/TestCases/TestValidatingModule.java
+++ src/TestCases/TestValidatingModule.java
@@ -6,51 +6,143 @@ import sourceCode.ValidatingModule;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-
 public class TestValidatingModule {	
+	int i=0;
+	int j=0;
 	@Test
-	public void testCheckparams() 
+	public void testValidateParams() 
 	{
+		System.out.println("*************************************************************");
+		System.out.println("TEST CASES FOR VALIDATING IF THE PARAMETERS ARE <=0 OR NOT");
+		System.out.println("\n");
 		assertEquals(true,ValidatingModule.checkParams(-1)); 
+		i=i+1;
+		System.out.println("Expected Result Is  " +ValidatingModule.checkParams(-1)+  "And The Result Is "+ true + "=> TESTCASE PASSED");
 		assertEquals(true,ValidatingModule.checkParams(0)); 
-		assertEquals(false,ValidatingModule.checkParams(1)); 		
+		i=i+1;
+		System.out.println("Expected Result Is  " +ValidatingModule.checkParams(0)+  "And The Result Is  "+ true + "=> TESTCASE PASSED");
+		assertEquals(false,ValidatingModule.checkParams(1)); 
+		i=i+1;
+		System.out.println("Expected Result Is  " +ValidatingModule.checkParams(1)+  "And The Result Is  "+ false + "=> TESTCASE PASSED");		
+		System.out.println("\n");
+		
+		assertEquals(true,ValidatingModule.checkParams(2)); 
+		j=j+1;
+		System.out.println("Expected Result Is  " +ValidatingModule.checkParams(2)+  "And The Result Is  "+ true + "=> TESTCASE FAILED");				
+		
 	}
 	
+	
 	@Test
-	public void testCheckSPO2() 
+	public void testValidateSPO2() 
 	{
+		System.out.println("*************************************************************");
+		System.out.println("TEST CASES FOR VALIDATING OXYGEN LEVELS");
+		System.out.println("\n");
 		assertEquals(false,ValidatingModule.checkSPO2(69));
+		i=i+1;
+		System.out.println("Expected Result Is  "+ValidatingModule.checkSPO2(69)+ ", The Result Is "+ false+ "=> TESTCASE PASSED");
+		
 		assertEquals(true,ValidatingModule.checkSPO2(70));
+		i=i+1;
+		System.out.println("Expected Result Is "+ValidatingModule.checkSPO2(70)+ ", The Result Is "+ true+ "=> TESTCASE PASSED");
+		
 		assertEquals(true,ValidatingModule.checkSPO2(71));
+		i=i+1;
+		System.out.println("Expected Result Is  "+ValidatingModule.checkSPO2(71)+ ", The Result Is "+ true+ "=> TESTCASE PASSED");
+				
 		assertEquals(true,ValidatingModule.checkSPO2(99));
+		i=i+1;
+		System.out.println("Expected Result Is  "+ValidatingModule.checkSPO2(99)+ ", The Result Is "+ true+ "=> TESTCASE PASSED");
+		
 		assertEquals(true,ValidatingModule.checkSPO2(100));
+		i=i+1;
+		System.out.println("Expected Result Is  "+ValidatingModule.checkSPO2(100)+ ", The Result Is "+ true+ "=> TESTCASE PASSED");
+		
 		assertEquals(false,ValidatingModule.checkSPO2(101));
+		i=i+1;
+		System.out.println("Expected Result Is  "+ValidatingModule.checkSPO2(101)+ ", The Result Is "+ false+ "=> TESTCASE PASSED");
+		System.out.println("\n");
+		
+		assertEquals(true,ValidatingModule.checkSPO2(102));
+		j=j+1;
+		System.out.println("Expected Result Is  "+ValidatingModule.checkSPO2(102)+ ", The Result Is "+ true+ "=> TESTCASE FAILED");
+		System.out.println("\n\n");
 		
 	}
 	
 	@Test
-	public void testCheckTemp()
+	public void testValidateTemp()
 	{
+		System.out.println("*************************************************************");
+		System.out.println("TEST CASES FOR VALIDATING TEMPERATURE");
+		System.out.println("\n");
 		assertEquals(false,ValidatingModule.checkTemp(92));
+		i=i+1;
+		System.out.println("Expected Result Is  "+ValidatingModule.checkTemp(92)+ ", The Result Is "+ false+ "=> TESTCASE PASSED");
+		
 		assertEquals(true,ValidatingModule.checkTemp(93));
+		i=i+1;
+		System.out.println("Expected Result Is  "+ValidatingModule.checkTemp(93)+ ", The Result Is "+ true+ "=> TESTCASE PASSED");
+		
 		assertEquals(true,ValidatingModule.checkTemp(94));
+		i=i+1;
+		System.out.println("Expected Result Is  "+ValidatingModule.checkTemp(94)+ ", The Result Is "+ true+ "=> TESTCASE PASSED");
+		
 		assertEquals(true,ValidatingModule.checkTemp(112));
+		i=i+1;
+		System.out.println("Expected Result Is  "+ValidatingModule.checkTemp(112)+ ", The Result Is "+ true+ "=> TESTCASE PASSED");
 		assertEquals(true,ValidatingModule.checkTemp(113));
+		i=i+1;
+		System.out.println("Expected Result Is  "+ValidatingModule.checkTemp(113)+ ", The Result Is "+ true+ "=> TESTCASE PASSED");
+		
 		assertEquals(false,ValidatingModule.checkTemp(114));
+		i=i+1;
+		System.out.println("Expected Result Is  "+ValidatingModule.checkTemp(114)+ ", The Result Is "+ false+ "=> TESTCASE PASSED");
+		System.out.println("\n");
+		assertEquals(true,ValidatingModule.checkTemp(114));
+		j=j+1;
+		System.out.println("Expected Result Is  "+ValidatingModule.checkTemp(114)+ ", The Result Is "+ true+ "=> TESTCASE FAILED");
+		
+		
 		
 	}
 	
 	
 	@Test
-	public void testCheckPulseRate() 
+	public void testValidatePulseRate() 
 	{
+		System.out.println("*************************************************************");
+		System.out.println("TEST CASES FOR VALIDATING HEARTRATE");
+		System.out.println("\n");
+		
 		assertEquals(false,ValidatingModule.checkPulseRate(29));
+		i=i+1;
+		System.out.println("Expected Result Is  "+ValidatingModule.checkPulseRate(29)+ ", The Result Is "+ false+ "=> TESTCASE PASSED");
+		
 		assertEquals(true,ValidatingModule.checkPulseRate(30));
+		i=i+1;
+		System.out.println("Expected Result Is  "+ValidatingModule.checkPulseRate(30)+ ", The Result Is "+ true+ "=> TESTCASE PASSED");
+		
 		assertEquals(true,ValidatingModule.checkPulseRate(31));
+		i=i+1;
+		System.out.println("Expected Result Is  "+ValidatingModule.checkPulseRate(31)+ ", The Result Is "+ true+ "=> TESTCASE PASSED");
+		
 		assertEquals(true,ValidatingModule.checkPulseRate(253));
+		i=i+1;
+		System.out.println("Expected Result Is  "+ValidatingModule.checkPulseRate(32)+ ", The Result Is "+ true+ "=> TESTCASE PASSED");
+		
 		assertEquals(true,ValidatingModule.checkPulseRate(254));
-		assertEquals(false,ValidatingModule.checkPulseRate(255));
+		i=i+1;
+		System.out.println("Expected Result Is  "+ValidatingModule.checkPulseRate(254)+ ", The Result Is "+ true+ "=> TESTCASE PASSED");
+		System.out.println("\n");
+		
+		assertEquals(true,ValidatingModule.checkPulseRate(255));
+		j=j+1;
+		System.out.println("Expected Result Is  "+ValidatingModule.checkPulseRate(255)+ ", The Result Is "+ true+ "=> TESTCASE FAILED");
+		
 		
 	}
+	
 	
 }
