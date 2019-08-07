3ef91c422d189b29b605ec8bffa8a1a8fdb46aac
HarshithaYeddula <33006130+HarshithaYeddula@users.noreply.github.com>
2019-08-07 17:53:53 +0530

    Update ValidatingModule.java

--- src/sourceCode/ValidatingModule.java
+++ src/sourceCode/ValidatingModule.java
@@ -2,13 +2,13 @@ package sourceCode;
 
 public class ValidatingModule  {
 	
-	public boolean validateDetails(Sample[] sample) {
+	public boolean validateDetails(PatientVitals[] sample) {
 		
 	String patientId = null;	
-	String spo2 = null;
-	String pulserate = null;
-	String temp = null;
-    boolean flag = false;
+	int spo2 = 0;
+	int pulserate = 0;
+	double temp = 0;
+        boolean flag = false;
 
 	    	
 	for (int i = 0; i < sample.length; i++) 
@@ -20,84 +20,80 @@ public class ValidatingModule  {
 		     temp = sample[i].temperature;		
 		
 	}
-	int spo2_=Integer.parseInt(spo2); 
-	int pulseRate = Integer.parseInt(pulserate);
-	double temperature = Double.parseDouble(temp);
+
 		
-	if(patientId == null){
+	if(patientId == null)
+	{
 		flag = true;
-		System.out.println("INVALID PATIENTID");}
-	else if(checkParams(spo2_)==true){
+		System.out.println("INVALID PATIENTID!!!");
+	}
+	else if(checkParams(spo2)==true)
+	{
 		flag=true;
-	    System.out.println("INVALID OXYGEN LEVELS");}
-	else if(checkParams(pulseRate)==true){
+	    System.out.println("INVALID OXYGEN LEVELS!!!");
+	}
+	else if(checkParams(pulserate)==true)
+	{
 		flag=true;
-		System.out.println("INVALID HEART RATE");}
-	else if(checkParams(temperature)==true){
+		System.out.println("INVALID HEART RATE!!!");
+	}
+	else if(checkParams(temp)==true)
+	{
 		flag=true;
-		System.out.println("INVALID TEMPERATURE");}
-	else if(checkSPO2(spo2_) == false){
+		System.out.println("INVALID TEMPERATURE!!!");
+	}
+	else if(checkSPO2(spo2) == false)
+	{
 		flag=true;
-		System.out.println("OXYGEN LEVEL IS OUT OF RANGE");}
-	else if(checkPulseRate(pulseRate)==false){
+		System.out.println("MACHINE ERROR----OXYGEN LEVEL IS OUT OF RANGE!!!");
+	}
+	else if(checkPulseRate(pulserate)==false)
+	{
 		flag=true;
-		System.out.println("PULSERATE IS OUT OF RANGE");}
-	else if(checkTemp(temperature) == false){
+		System.out.println("MACHINE ERROR----PULSERATE IS OUT OF RANGE!!!");
+	}
+	else if(checkTemp(temp) == false)
+	{
 		flag=true;
-		System.out.println("TEMPERATURE IS OUT OF RANGE");}
+		System.out.println("MACHINE ERROR----TEMPERATURE IS OUT OF RANGE!!!");
+	}
 	else
 		flag = false;
 	
 	
 	return flag;
 	
-	}
-
-	
+	}	
 	
 	// Method1
-    public static boolean checkParams(double x)
-    {		
-		if(x<=0) 
-			return true;		
-		else 
-			return false;		
-	}
+    public static boolean checkParams(double x) { 		
+		return(x<=0);}		
+	
     
     //Method 2
     public static boolean checkSPO2(double spo2)
     {
-    	if(spo2>=70 && spo2<=100)
-    		return true;
-    	else
-    		return false;
-    	
+    	return(spo2>=70 && spo2<=100);
     }
-    
     
   //Method 3
     public static boolean checkTemp(double temperature)
     {
-    	if(temperature>=93 && temperature<=113)
-    		return true;
-    	else
-    		return false;
-    	
+    	return(temperature>=93 && temperature<=113);
     }
     
     //Method 4
     public static boolean checkPulseRate(int pulseRate) 
     {
-		if(pulseRate>=30 && pulseRate<=254)
-			return true;
-		else
-			return false;
-	}
-    
-	
-	
+		return(pulseRate>=30 && pulseRate<=254);
+    }
+		
 
 }
+		
+
+
+
 		
 
 
